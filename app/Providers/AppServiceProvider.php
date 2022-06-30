97d04b74ed316fd44a7a04a53c3c1eae131a7d37
adrianidiot <salguetadrian1@gmail.com>
2022-06-30 14:42:33 +0800

    remove pwd and 4ps menu

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,9 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (app()->environment(['production'])) {
-            $this->app['request']->server->set('HTTPS', true);
-            URL::forceScheme('https');
-        }
+        $this->app['request']->server->set('HTTPS', true);
+        URL::forceScheme('https');
     }
 }
