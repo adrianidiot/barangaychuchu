f8d8ea0ccd8bf4e8c0fde42a7635a6066a60dfe6
adrianidiot <salguetadrian1@gmail.com>
2022-07-12 16:14:19 +0800

    adding vaccine status

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // $this->app['request']->server->set('HTTPS', true);
-        // URL::forceScheme('https');
+        $this->app['request']->server->set('HTTPS', true);
+        URL::forceScheme('https');
     }
 }
